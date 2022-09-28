e91037c90776da40ca3a42b72add386d693e34dc
AleksandarIlicc <aleksandari336@gmail.com>
2022-09-29 00:00:45 +0200

    Publish to Heroku

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_URL_LOCAL);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
