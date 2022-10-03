3d22aaf27791c1812d83732baf630a12f3aed2f4
AleksandarIlicc <aleksandari336@gmail.com>
2022-10-03 21:05:42 +0200

    Add Footer component

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import ShoppingInfoBar from "../components/ShoppingInfoBar";
 import ImageGallery from "../components/ImageGallery";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Header />
       <ShoppingInfoBar />
       <ImageGallery />
+      <Footer />
     </>
   );
 };
